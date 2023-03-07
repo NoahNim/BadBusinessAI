c0630fc23b6fc8ea9ccf236076b12a32e092069f
noahnim <noahmedoff@gmail.com>
2023-03-07 11:02:18 -0800

    working on setup..

---
+++ backend/config/database.js
@@ -0,0 +1,36 @@
+const config = require('./index');
+
+const db = config.db;
+const jwtConfig = config.jwtConfig
+const username = db.username;
+const password = db.password;
+const database = db.database;
+const host = db.host;
+const jwtSecret = jwtConfig.secret;
+
+module.exports = {
+  development: {
+    username,
+    password,
+    database,
+    host,
+    jwtSecret,
+    dialect: 'postgres',
+    seederStorage: 'sequelize',
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    seederStorage: 'sequelize',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+    define: {         // define schema here
+      schema: process.env.SCHEMA
+    } //it is here
+  },
+};
+   // "build": "npm run build --prefix backend"
