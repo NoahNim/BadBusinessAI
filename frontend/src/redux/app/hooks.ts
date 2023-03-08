4abe5e131baea8341818a50e505b6d3a251c8fe6
noahnim <noahmedoff@gmail.com>
2023-03-07 16:54:39 -0800

    user slice coded and imported into store

--- frontend/src/redux/app/hooks.ts
+++ frontend/src/redux/app/hooks.ts
@@ -4,3 +4,12 @@ import type { RootState, AppDispatch } from './store';
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+
+export const getCSRFCookie = (name: string) => {
+    if (name) {
+        const cookieValue = document.cookie.split("; ").find((row) => row.startsWith(name))?.split('=')[1]
+
+        return cookieValue;
+    }
+}
---
+++ frontend/src/redux/app/services/api.ts
@@ -0,0 +1,28 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { url } from 'inspector';
+import { getCSRFCookie } from '../hooks';
+
+export interface User {
+    id: number;
+    username: string;
+    email: string;
+    password: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+export interface UserResponse {
+    user: User;
+    token: string | null | undefined;
+    userImage: Buffer | null;
+}
+
+export interface LoginRequest {
+    credential: string;
+    password: string;
+}
+
+export interface restoreRequest {
+    user: User;
+}
+
