de94052ad38bac9d74892a96d246a106c81065b2
noahnim <noahmedoff@gmail.com>
2023-03-07 17:20:03 -0800

    write login api endpoint mutation

--- frontend/src/redux/app/services/api.ts
+++ frontend/src/redux/app/services/api.ts
@@ -26,7 +26,10 @@ export interface restoreRequest {
     user: User;
 }
 
+
+// this is where the rtk query functions are created and passed from. RTK Query makes an async call to the route in the server
 export const api = createApi({
+    // the base query is the first query made when a user goes to the home url, in this case "/"
     baseQuery: fetchBaseQuery({
         baseUrl: '/',
         prepareHeaders: async (headers, endpoints) => {
@@ -36,10 +39,25 @@ export const api = createApi({
                 headers.set('XSRF-TOKEN', authToken);
             }
 
+            headers.set('Content-type', 'application/json')
+
             return headers
         }
     }),
+    // endpoints are the other queries to the API
     endpoints: (builder) => ({
 
+        // endpoint for csrfrestore
+        restore: builder.query({  // builder.query creates an async function which makes a query to the api on the server
+            query: () => 'api/csrf/restore'
+        }),
+        // login endpoint
+        login: builder.mutation<UserResponse, LoginRequest>({ //builder.mutation allows for the query to send updates to the server and apply the changes to the local cache, it also checks that the request and response are the appropriate data Types
+            query: (credentials) => ({
+                url: '/api/users/log-in',
+                method: 'POST',
+                body: JSON.stringify(credentials)
+            })
+        }),
     })
 })
