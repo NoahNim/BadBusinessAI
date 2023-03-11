768006e52c32a5fec85bc09c81a332ca28d17ea2
noahnim <noahmedoff@gmail.com>
2023-03-11 12:50:39 -0800

    create api call to openai chaptgpt

--- backend/routes/api/badideas.js
+++ backend/routes/api/badideas.js
@@ -1,9 +1,6 @@
 const { Configuration, OpenAIApi } = require("openai");
 const express = require("express");
-const { check } = require("express-validator");
 const asyncHandler = require("express-async-handler");
-const { handleValidationErrors } = require("../../utils/validation");
-const { setTokenCookie, restoreUser } = require("../../utils/auth");
 const { BadIdea } = require("../../db/models");
 const router = express.Router();
 
@@ -11,3 +8,16 @@ const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
+
+
+router.post('/chatgpt', asyncHandler(async (req, res) => {
+    const res = await openai.createCompletion({
+        modal: "gpt-3.5-turbo",
+        prompt: "Give me a bad business idea and create a pitch for that bad business idea."
+    })
+    return res.json();
+}))
+
+
+
+module.exports = router;
--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const usersRouter = require("./users.js");
+const badideasRouter = require("./badideas.js")
 
 router.use("/users", usersRouter);
+router.use("/badideas", badideasRouter)
 
 module.exports = router;
