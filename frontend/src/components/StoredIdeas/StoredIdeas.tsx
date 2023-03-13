b74664be1360831e91c4381f3b31525265550cc0
noahnim <noahmedoff@gmail.com>
2023-03-13 15:24:12 -0700

    working on styling

--- frontend/src/components/StoredIdeas/StoredIdeas.tsx
+++ frontend/src/components/StoredIdeas/StoredIdeas.tsx
@@ -1,22 +1,22 @@
-import { useEffect } from "react";
 import { useGetStoredBadIdeasQuery } from "../../redux/app/services/api";
 import { useAppSelector } from "../../redux/app/hooks";
+import { Card, Container } from "react-bootstrap";
 
 export const StoredBadIdeas = () => {
     const sessionUser = useAppSelector((state) => state?.auth?.user)
     const { data: storedIdeasList } = useGetStoredBadIdeasQuery({});
     return (sessionUser ?
-        <div className={"d-flex flex-column justify-content-center w-90"}>
+        <Container className={"d-flex flex-direction-row justify-content-start align-items-center flex-wrap"} fluid>
             {storedIdeasList?.map((idea: any) => {
                 return (
-                    <div key={idea.id}>
-                        <div>
+                    <Card className={"d-flex flex-direction-column justify-content-center align-items-center border border-primary-subtle text-wrap"} style={{ width: "25%", height: "10%" }} key={idea.id}>
+                        <Card.Body className={"overflow-y-scroll"} style={{ height: "10%" }} >
                             {idea.idea}
-                        </div>
-                    </div>
+                        </Card.Body>
+                    </Card>
                 )
             })}
-        </div>
+        </Container>
         :
         <div>
             Unauthorized, please log in or sign up.
