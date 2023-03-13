594d4a1926655e71cd06bf496e177ca0e09606b1
noahnim <noahmedoff@gmail.com>
2023-03-12 17:28:18 -0700

    margin with button

--- frontend/src/components/Home/Home.tsx
+++ frontend/src/components/Home/Home.tsx
@@ -25,7 +25,7 @@ export const Home = () => {
                 {!isLoading && !badidea ? <div className={"d-flex flex-column justify-content-center align-items-center"}><div className={"w-50"}>Click the button below to get a bad business idea. Please be aware that sometimes the AI model might generate a response it considers harmful, and in that case you will get a warning saying it can't tell you something harmful. Enjoy!</div></div> : null}
                 {!isLoading ? <div>{badidea?.data}</div> : <div className={"align-items-center"}>Loading...</div>}
             </div >
-            <Button onClick={getBadIdeaHandler}>{!badidea ? "Get A Bad Business Idea" : "Get Another Idea"}</Button>
+            <Button style={{ marginTop: "1%" }} onClick={getBadIdeaHandler}>{!badidea ? "Get A Bad Business Idea" : "Get Another Idea"}</Button>
         </div >
     )
 }
